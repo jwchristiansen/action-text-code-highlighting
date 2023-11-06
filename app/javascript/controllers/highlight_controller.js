a18b0b3fb75afde2da533bfe1b190f9956658263
Joel Christiansen <joel@joelc.io>
2023-11-06 14:14:20 -0800

    specify language; add plaintext

--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import hljs from 'highlight.js/lib/core';
+import plaintext from 'highlight.js/lib/languages/plaintext';
 import javascript from 'highlight.js/lib/languages/javascript';
 import erb from 'highlight.js/lib/languages/erb';
 import ruby from 'highlight.js/lib/languages/ruby';
@@ -8,6 +9,7 @@ import xml from 'highlight.js/lib/languages/xml';
 // Connects to data-controller="highlight"
 export default class extends Controller {
   connect() {
+    hljs.registerLanguage('plaintext', plaintext);
     hljs.registerLanguage('javascript', javascript);
     hljs.registerLanguage('erb', erb);
     hljs.registerLanguage('ruby', ruby);
