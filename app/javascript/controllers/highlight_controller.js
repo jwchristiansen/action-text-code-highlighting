c4f8a0ebd759ae211b85e17fc79c22cbb7cb8aa7
Joel Christiansen <joel@joelc.io>
2023-11-05 18:20:31 -0800

    highlighted code blocks

---
+++ app/javascript/controllers/highlight_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import erb from 'highlight.js/lib/languages/erb';
+import ruby from 'highlight.js/lib/languages/ruby';
+import xml from 'highlight.js/lib/languages/xml';
+
+// Connects to data-controller="highlight"
+export default class extends Controller {
+  connect() {
+    hljs.registerLanguage('javascript', javascript);
+    hljs.registerLanguage('erb', erb);
+    hljs.registerLanguage('ruby', ruby);
+    hljs.registerLanguage('xml', xml);
+
+    this.element.querySelectorAll('pre').forEach((element) => {
+      hljs.highlightElement(element);
+    });
+  }
+}
